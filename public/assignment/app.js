4878ab0d7126e779043fd693b307f71bcac2be59
Ben Dunbar <dunbarb@ccs.neu.edu>
2016-05-23 14:51:01 -0400

    Create angular app.

---
+++ public/assignment/app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Ben on 5/23/16.
+ */
+
+/**
+ * Keep code in a function so that it isn't interacting with global variables.
+ */
+(function () {
+    angular.module("WebAppMaker", []);
+})();
