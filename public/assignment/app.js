e40d858a5f48370be2a61e85c5bc6e36f4103e9b
Ben Dunbar <dunbarb@ccs.neu.edu>
2016-05-23 14:55:02 -0400

    Create Views Directory

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -4,6 +4,7 @@
 
 /**
  * Keep code in a function so that it isn't interacting with global variables.
+ * This design patter is IIFE - Immediately Invoked Function Environment.
  */
 (function () {
     angular.module("WebAppMaker", []);
