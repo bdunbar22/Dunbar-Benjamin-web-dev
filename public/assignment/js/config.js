1185b56eb77518e25450c389b1ff84e57daeda96
Ben Dunbar <dunbarb@ccs.neu.edu>
2016-05-23 18:40:50 -0400

    Remove head, meta, script, link, html, and body tags from templates.

--- public/assignment/js/config.js
+++ public/assignment/js/config.js
@@ -11,11 +11,26 @@
             .when("/login", {
                 templateUrl: "../views/user/login.view.client.html"
             })
+            .when("/", {
+                templateUrl: "../views/user/login.view.client.html"
+            })
+            .when("/default", {
+                templateUrl: "../views/user/login.view.client.html"
+            })
             .when("/register", {
                 templateUrl: "../views/user/register.view.client.html"
             })
-            .when("/profile", {
+            .when("/user/:uid", {
                 templateUrl: "../views/user/profile.view.client.html"
+            })
+            .when("/user/:uid/website", {
+                templateUrl: "../views/website/website-list.view.client.html"
+            })
+            .when("/user/:uid/website/new", {
+                templateUrl: "../views/website/website-new.view.client.html"
+            })
+            .when("/user/:uid/website/:wid", {
+                templateUrl: "../views/website/website-edit.view.client.html"
             });
     }
 })();
