3ec95773295b1083e02201c2ae0fd33d7f48cc12
Ben Dunbar <dunbarb@ccs.neu.edu>
2016-05-23 18:30:36 -0400

    Move javascript files into a javascript directory to organize.

--- public/assignment/js/config.js
+++ public/assignment/js/config.js
@@ -9,13 +9,13 @@
     function Config($routeProvider) {
         $routeProvider
             .when("/login", {
-                templateUrl: "views/user/login.view.client.html"
+                templateUrl: "../views/user/login.view.client.html"
             })
             .when("/register", {
-                templateUrl: "views/user/register.view.client.html"
+                templateUrl: "../views/user/register.view.client.html"
             })
             .when("/profile", {
-                templateUrl: "views/user/profile.view.client.html"
+                templateUrl: "../views/user/profile.view.client.html"
             });
     }
 })();
