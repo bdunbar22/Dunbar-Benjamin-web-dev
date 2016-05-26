e732ba5f7c4f7aacbd8a710ae39a59eb578af1ac
Ben Dunbar <dunbarb@ccs.neu.edu>
2016-05-26 15:01:47 -0400

    Start using sce to trust html and image/youtube urls.

--- public/assignment/js/controller/widget-list.controller.client.js
+++ public/assignment/js/controller/widget-list.controller.client.js
@@ -7,10 +7,26 @@
         .module("WebAppMaker")
         .controller("WidgetListController", WidgetListController);
 
-    function WidgetListController($routeParams, WidgetService) {
+    function WidgetListController($routeParams, $sce, WidgetService) {
         var vm = this;
         var pageId = $routeParams.pid;
 
         vm.widgets = WidgetService.findByPageId(pageId);
+
+        vm.getTrustedHtml = getTrustedHtml;
+        vm.getTrustedUrl = getTrustedUrl;
+
+        function getTrustedHtml(widget) {
+            var html = $sce.trustAsHtml(widget.txt);
+            return html;
+        }
+
+        function getTrustedUrl(widget) {
+            var urlParts = widget.url.split("/");
+            var urlId = urlParts[urlParts.length - 1];
+            var urlToEmbed = "https://www.youtube.com/embed/" + urlId;
+            $sce.trustAsResourceUrl(urlToEmbed);
+            return urlToEmbed;
+        }
     }
 })();
