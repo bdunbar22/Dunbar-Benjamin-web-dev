e4ac70abee67b7e25a6483567bd92e9dc96044d2
Ben Dunbar <dunbarb@ccs.neu.edu>
2016-05-29 00:40:29 -0400

    Finish website service.

--- public/assignment/js/service/website.service.client.js
+++ public/assignment/js/service/website.service.client.js
@@ -20,34 +20,87 @@
 
         var api = {
             createWebsite: createWebsite,
-            findWebsitesByDeveloperId: findWebsitesByDeveloperId,
-            findWebsitesById: findWebsitesById
+            findWebsitesByUser: findWebsitesByUser,
+            findWebsiteById: findWebsiteById,
+            updateWebsite: updateWebsite,
+            deleteWebsite: deleteWebsite
         };
         return api;
 
-        function createWebsite() {
-            //TODO: implement
+        /**
+         * Adds the website parameter instance to the local websites array. The new website's developerId is
+         * set to the  userId parameter.
+         * @param userId
+         * @param website
+         * @returns {null}
+         */
+        function createWebsite(userId, website) {
+            website.developerId = userId;
+            websites.push(website);
             return null;
         }
 
-        function findWebsitesByDeveloperId(id) {
+        /**
+         * Retrieves the websites in local websites array whose developerId matches the parameter  userId.
+         * @param userId
+         * @returns {Array} websites
+         */
+        function findWebsitesByUser(userId) {
             var websitesForUser = [];
             for(var i in websites) {
-                if(websites[i].developerId === id) {
+                if(websites[i].developerId === userId) {
                     websitesForUser.push(websites[i]);
                 }
             }
             return websitesForUser;
         }
 
-        function findWebsitesById(id) {
-            var websitesForId = [];
+        /**
+         * Retrieves the website in local websites array whose _id matches the websiteId parameter.
+         * @param websiteId
+         * @returns null if no website found.
+         */
+        function findWebsiteById(websiteId) {
             for(var i in websites) {
-                if(websites[i]._id === id) {
-                    websitesForId.push(websites[i]);
+                if(websites[i]._id === websiteId) {
+                    return websites[i];
                 }
             }
-            return websitesForId;
+            return null;
+        }
+
+        /**
+         * Updates the website in local websites array whose _id matches the websiteId parameter.
+         * @param websiteId
+         * @param website
+         * @returns {boolean} true if updated.
+         */
+        function updateWebsite(websiteId, website) {
+            for(var i in websites) {
+                if(websites[i]._id === websiteId) {
+                    websites[i].name = website.name;
+                    websites[i].developerId = website.developerId;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /**
+         * Removes the website from local websites array whose  _id matches the websiteId parameter.
+         * @param websiteId
+         * @returns {boolean}
+         */
+        function deleteWebsite(websiteId) {
+            var startLength = websites.length;
+            websites.filter(checkId);
+
+            //Items that pass this check will remain in the list.
+            function checkId(website) {
+                return website._id != websiteId;
+            }
+
+            return websites.length < startLength;
         }
     }
 })();
