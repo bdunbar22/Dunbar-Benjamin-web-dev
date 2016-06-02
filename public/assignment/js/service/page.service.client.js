155697cb955495477e0484272fd6382f1a844e4d
Ben Dunbar <dunbarb@ccs.neu.edu>
2016-06-01 23:50:14 -0400

    Update client service to use server api endpoints.

--- public/assignment/js/service/page.service.client.js
+++ public/assignment/js/service/page.service.client.js
@@ -1,5 +1,7 @@
 /**
  * Created by Ben on 5/29/16.
+ * Allows for API calls to the server to give CRUD operations.
+ * Gives operations for the Page services.
  */
 
 
@@ -8,14 +10,7 @@
         .module("WebAppMaker")
         .factory("PageService", PageService);
 
-    function PageService() {
-        var pages =
-            [
-                { "_id": "321", "name": "Post 1", "websiteId": "456" },
-                { "_id": "432", "name": "Post 2", "websiteId": "456" },
-                { "_id": "543", "name": "Post 3", "websiteId": "456" }
-            ];
-
+    function PageService($http) {
         var api = {
             createPage: createPage,
             findPagesByWebsiteId: findPagesByWebsiteId,
@@ -25,85 +20,29 @@
         };
         return api;
 
-        /**
-         * Adds the page parameter instance to the local pages array. The new page's websiteId is set to the
-         * websiteId parameter.
-         * @param websiteId
-         * @param page
-         * @returns {boolean} true if created.
-         */
         function createPage(websiteId, page) {
-            if(!findPageById(page._id)) {
-                page.websiteId = websiteId;
-                pages.push(page);
-                return true;
-            }
-            return false;
+            var url = "/api/website/" + websiteId + "/page";
+            return $http.post(url, page);
         }
 
-        /**
-         * Retrieves the pages in the local pages array whose websiteId match the given websiteId
-         * @param websiteId
-         */
         function findPagesByWebsiteId(websiteId) {
-            var pagesForId = [];
-            for(var i in pages) {
-                if(pages[i].websiteId == websiteId) {
-                    pagesForId.push(pages[i]);
-                }
-            }
-            return pagesForId;
+            var url = "/api/website/" + websiteId + "/page";
+            return $http.get(url);
         }
 
-        /**
-         * Retrieves the page in local pages array whose _id matches the pageId parameter.
-         * @param pageId
-         * @returns {*} page if found. Null if not.
-         */
         function findPageById(pageId) {
-            for(var i in pages) {
-                if(pages[i]._id == pageId) {
-                    return pages[i];
-                }
-            }
-            return null;
+            var url = "/api/page/" + pageId;
+            return $http.get(url);
         }
 
-        /**
-         * Updates the page in local pages array whose  _id matches the pageId parameter.
-         * @param pageId
-         * @param page
-         * @returns {boolean} true if updated.
-         */
         function updatePage(pageId, page) {
-            for(var i in pages) {
-                if(pages[i]._id == pageId) {
-                    pages[i].name = page.name;
-                    pages[i].websiteId = page.websiteId;
-                    return true;
-                }
-            }
-            return false;
+            var url = "/api/page/" + pageId;
+            return $http.put(url, page);
         }
 
-        /**
-         * Removes the page from local pages array whose _id matches the pageId parameter.
-         * @param pageId
-         * @returns {boolean} true if found a page to delete.
-         */
         function deletePage(pageId) {
-            var startLength = pages.length;
-
-            var keepPages = [];
-            for(var i in pages) {
-                if(pages[i]._id != pageId) {
-                    keepPages.push(pages[i]);
-                }
-            }
-
-            pages = keepPages;
-
-            return pages.length < startLength;
+            var url = "/api/page/" + pageId;
+            return $http.delete(url);
         }
     }
 })();
