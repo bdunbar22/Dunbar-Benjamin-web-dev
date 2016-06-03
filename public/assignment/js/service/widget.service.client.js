5ad4680eac6520983bb42c48429b8956a8d5c50c
Ben Dunbar <dunbarb@ccs.neu.edu>
2016-06-03 11:21:04 -0400

    Update widget client service to use api endpoints of server.

--- public/assignment/js/service/widget.service.client.js
+++ public/assignment/js/service/widget.service.client.js
@@ -1,5 +1,7 @@
 /**
  * Created by Ben on 5/26/16.
+ * Allows for API calls to the server to give CRUD operations.
+ * Gives operations for the Widget services.
  */
 
 (function() {
@@ -8,19 +10,6 @@
         .factory("WidgetService", WidgetService);
 
     function WidgetService() {
-        var widgets =
-            [
-                { "_id": "123", "widgetType": "HEADER", "pageId": "321", "size": 2, "text": "GIZMODO"},
-                { "_id": "234", "widgetType": "HEADER", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
-                { "_id": "345", "widgetType": "IMAGE", "pageId": "321", "width": "100%",
-                    "url": "http://lorempixel.com/400/200/"},
-                { "_id": "456", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"},
-                { "_id": "567", "widgetType": "HEADER", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
-                { "_id": "678", "widgetType": "YOUTUBE", "pageId": "321", "width": "100%",
-                    "url": "https://youtu.be/AM2Ivdi9c4E" },
-                { "_id": "789", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"}
-            ];
-
         var api = {
             createWidget: createWidget,
             findWidgetsByPageId: findWidgetsByPageId,
@@ -30,98 +19,29 @@
         };
         return api;
 
-        /**
-         * Adds the widget parameter instance to the local widgets array.
-         * The new widget's pageId is set to the pageId parameter.
-         * @param pageId
-         * @param widget
-         * @returns {boolean} true if created.
-         */
         function createWidget(pageId, widget) {
-            if(!findWidgetById(widget._id)) {
-                widget.pageId = pageId;
-                widgets.push(widget);
-                return true;
-            }
-            return false;
+            var url = "/api/page/" + pageId + "/widget";
+            return $http.post(url, widget);
         }
 
-        /**
-         * Retrieves the widgets in local widgets array whose pageId match the parameter pageId.
-         * @param pageId
-         * @returns {Array}
-         */
         function findWidgetsByPageId(pageId) {
-            var widgetsForPage = [];
-            for(var i in widgets) {
-                if(widgets[i].pageId == pageId) {
-                    widgetsForPage.push(widgets[i]);
-                }
-            }
-            return widgetsForPage;
+            var url = "/api/page/" + pageId + "/widget";
+            return $http.get(url);
         }
 
-        /**
-         * Retrieves the widget in local widgets array whose _id matches the widgetId parameter.
-         * @param widgetId
-         * @returns {*}
-         */
         function findWidgetById(widgetId) {
-            for(var i in widgets) {
-                if(widgets[i]._id == widgetId) {
-                    return widgets[i];
-                }
-            }
-            return null;
+            var url = "/api/widget/" + widgetId;
+            return $http.get(url);
         }
 
-        /**
-         * Updates the widget in local widgets array whose _id matches the widgetId parameter.
-         * @param widgetId
-         * @param widget
-         * @returns {boolean} true if updated.
-         */
         function updateWidget(widgetId, widget) {
-            for(var i in widgets) {
-                if(widgets[i]._id == widgetId) {
-                    widgets[i].widgetType = widget.widgetType;
-                    widgets[i].pageId = widget.pageId;
-
-                    //Some attributes depend on type.
-                    if(widget.widgetType === "HEADER") {
-                        widgets[i].size = widget.size;
-                    }
-                    if(widget.widgetType === "HEADER" || widget.widgetType === "HTML") {
-                        widgets[i].text = widget.text;
-                    }
-                    if(widget.widgetType === "YOUTUBE" || widget.widgetType === "IMAGE") {
-                        widgets[i].width = widget.width;
-                        widgets[i].url = widget.url;
-                    }
-                    return true;
-                }
-            }
-            return false;
+            var url = "/api/widget/" + widgetId;
+            return $http.put(url, widget);
         }
 
-        /**
-         * Removes the widget from local widgets array whose _id matches the widgetId parameter.
-         * @param widgetId
-         * @returns {boolean} true if removed.
-         */
         function deleteWidget(widgetId) {
-            var startLength = widgets.length;
-
-            var keepWidgets = [];
-            for(var i in widgets) {
-                if(widgets[i]._id != widgetId) {
-                    keepWidgets.push(widgets[i]);
-                }
-            }
-
-            widgets = keepWidgets;
-
-            return widgets.length < startLength;
+            var url = "/api/widget/" + widgetId;
+            return $http.delete(url);
         }
     }
 })();
