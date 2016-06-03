f6afd62eedac1c4d11750b8a6d0c46bec99012d1
Ben Dunbar <dunbarb@ccs.neu.edu>
2016-06-03 11:35:13 -0400

    widget service needs $http to be sent to function.

--- public/assignment/js/service/widget.service.client.js
+++ public/assignment/js/service/widget.service.client.js
@@ -9,7 +9,7 @@
         .module("WebAppMaker")
         .factory("WidgetService", WidgetService);
 
-    function WidgetService() {
+    function WidgetService($http) {
         var api = {
             createWidget: createWidget,
             findWidgetsByPageId: findWidgetsByPageId,
