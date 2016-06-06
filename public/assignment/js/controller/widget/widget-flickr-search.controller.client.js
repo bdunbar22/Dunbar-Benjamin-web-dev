7a02dbfae88afcad81c45d2a9872de6169deb6a2
Ben Dunbar <dunbarb@ccs.neu.edu>
2016-06-05 22:33:21 -0400

    Flickr image can be chosen and automatically updates widget.

--- public/assignment/js/controller/widget/widget-flickr-search.controller.client.js
+++ public/assignment/js/controller/widget/widget-flickr-search.controller.client.js
@@ -7,9 +7,23 @@
         .module("WebAppMaker")
         .controller("FlickrImageSearchController", FlickrImageSearchController);
 
-    function FlickrImageSearchController(FlickrService) {
+    function FlickrImageSearchController($routeParams, $location, FlickrService, WidgetService) {
         var vm = this;
         vm.searchPhotos = searchPhotos;
+        vm.selectPhoto = selectPhoto;
+
+        function init() {
+            vm.userId = $routeParams["uid"];
+            vm.websiteId = $routeParams["wid"];
+            vm.pageId = $routeParams["pid"];
+            vm.widgetId = $routeParams["wgid"];
+            WidgetService
+                .findWidgetById(vm.widgetId)
+                .then(function (resp) {
+                    vm.widget = resp.data;
+                });
+        }
+        init();
 
         function searchPhotos(searchText) {
             FlickrService
@@ -25,6 +39,18 @@
                         vm.error = error.data;
                     }
                 );
+        }
+
+        //Update the url to be the url of the chosen photo.
+        function selectPhoto(photo) {
+            vm.widget.url = "https://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id +
+                            "_" + photo.secret + "_s.jpg";
+            WidgetService.updateWidget(vm.widgetId, vm.widget)
+                .then(function (resp) {
+                    $location.url("/user/" + vm.userId + "/website/" + vm.websiteId + "/page/" + vm.pageId + "/widget");
+                }, function (error) {
+                    vm.error = error.data;
+                });
         }
     }
 })();
