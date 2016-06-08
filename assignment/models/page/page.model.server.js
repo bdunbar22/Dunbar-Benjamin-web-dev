de6423bd858b7f472b63265c7b91f83c3b343cb1
Ben Dunbar <dunbarb@ccs.neu.edu>
2016-06-07 23:17:48 -0400

    Create models with functions.

--- assignment/models/page/page.model.server.js
+++ assignment/models/page/page.model.server.js
@@ -1,3 +1,39 @@
 /**
  * Created by Ben on 6/6/16.
  */
+
+module.exports = function () {
+    var mongoose = require("mongoose");
+    var PageSchema = require("./page.schema.server")();
+    var Page = mongoose.model("Page", PageSchema);
+
+    var api = {
+        createPage: createPage,
+        findPagesByWebsiteId: findPagesByWebsiteId,
+        findPageById: findPageById,
+        updatePage: updatePage,
+        deletePage: deletePage
+    };
+    return api;
+
+    function createPage(websiteId, page) {
+        page._website = websiteId;
+        return Page.create(page);
+    }
+
+    function findPagesByWebsiteId(websiteId) {
+        return Page.find({_website: websiteId});
+    }
+
+    function findPageById(pageId) {
+        return Page.findById(pageId);
+    }
+
+    function updatePage(pageId, page) {
+        return Page.update({_id: pageId}, {$set: page});
+    }
+
+    function deletePage(pageId) {
+        return Page.remove({_id: pageId});
+    }
+};
