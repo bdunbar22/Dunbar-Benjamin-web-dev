8a7b9d6e21b22c9d3237babdcc7539e000adfad6
Ben Dunbar <dunbarb@ccs.neu.edu>
2016-06-08 02:24:30 -0400

    Connect for openshift as well.

--- assignment/models/models.server.js
+++ assignment/models/models.server.js
@@ -5,7 +5,6 @@
 module.exports = function () {
     var mongoose = require('mongoose');
     var connectionString = 'mongodb://127.0.0.1:27017/assignment';
-    mongoose.connect(connectionString);
 
     if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
         connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
@@ -14,6 +13,8 @@ module.exports = function () {
             process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
             process.env.OPENSHIFT_APP_NAME;
     }
+
+    mongoose.connect(connectionString);
 
     var userModel = require("./user/user.model.server")();
     var websiteModel = require("./website/website.model.server")();
