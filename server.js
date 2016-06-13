45f88f58d2de125b3fd7fa51c67c22457094caf9
Ben Dunbar <dunbarb@ccs.neu.edu>
2016-06-13 14:34:48 -0400

    update and save dependencies.

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var seession = require('express-session');
+var passport = require('passport');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
