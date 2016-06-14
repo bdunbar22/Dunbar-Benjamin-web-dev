5194824befdef3753c38c8d677892ebdec3628d4
Ben Dunbar <dunbarb@ccs.neu.edu>
2016-06-14 13:37:10 -0400

    Use cookie parser and express session

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ var app = express();
 
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
-var seession = require('express-session');
+var session = require('express-session');
 var passport = require('passport');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(session({ secret: process.env.SESSION_SECRET }));
+
 
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
